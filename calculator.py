327b5d4057307242646b10cbd57926d430083f02
Diego Havenstein <diego@ww-lab.de>
2025-01-16 15:27:50 +0100

    Add substraction functionality

--- calculator.py
+++ calculator.py
@@ -2,6 +2,8 @@ def calculate(text_input):
     print("Welcome to our calculator")
     if text_input[1] == "+":
         return int(text_input[0])+int(text_input[-1])
+    if text_input[1] == "-":
+        return int(text_input[0]) - int(text_input[-1])
 
 if __name__ == "__main__":
     calculation = input("What do you want to calculate?")
